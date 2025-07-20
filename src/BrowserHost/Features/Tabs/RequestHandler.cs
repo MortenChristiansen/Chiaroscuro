a3434d88523e7725dc3c39fc5e86f6b835bc6a91
Morten Christiansen <withoutprecedence@gmail.com>
2025-07-20 12:06:57 +0200

    Middle mouse click opens links in new tab

--- src/BrowserHost/Features/Tabs/RequestHandler.cs
+++ src/BrowserHost/Features/Tabs/RequestHandler.cs
@@ -1,4 +1,6 @@
-﻿using CefSharp;
+﻿using BrowserHost.Features.ActionDialog;
+using BrowserHost.Utilities;
+using CefSharp;
 
 namespace BrowserHost.Features.Tabs;
 
@@ -7,5 +9,16 @@ public class RequestHandler(string tabId) : CefSharp.Handler.RequestHandler
     protected override IResourceRequestHandler GetResourceRequestHandler(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IRequest request, bool isNavigation, bool isDownload, string requestInitiator, ref bool disableDefaultHandling)
     {
         return new ResourceRequestHandler(tabId);
+    }
+
+    protected override bool OnOpenUrlFromTab(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, string targetUrl, WindowOpenDisposition targetDisposition, bool userGesture)
+    {
+        if (targetDisposition == WindowOpenDisposition.NewBackgroundTab)
+        {
+            PubSub.Publish(new NavigationStartedEvent(targetUrl, UseCurrentTab: false, SaveInHistory: true));
+            return true;
+        }
+
+        return false;
     }
 }
