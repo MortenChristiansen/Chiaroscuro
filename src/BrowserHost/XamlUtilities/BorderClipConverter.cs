a5ae012ccd810db43a601ab4ceb08484d2e97b84
Morten Christiansen <withoutprecedence@gmail.com>
2025-08-02 12:48:56 +0200

    Added utility to animate grid widths

--- src/BrowserHost/XamlUtilities/BorderClipConverter.cs
+++ src/BrowserHost/XamlUtilities/BorderClipConverter.cs
@@ -4,7 +4,7 @@ using System.Windows;
 using System.Windows.Data;
 using System.Windows.Media;
 
-namespace BrowserHost.Converter;
+namespace BrowserHost.XamlUtilities;
 
 internal class BorderClipConverter : IMultiValueConverter
 {
