80902e2ecf31a93fd0be6288ea53230eb4530edd
Morten Christiansen <withoutprecedence@gmail.com>
2026-01-18 12:10:06 +0100

    Added project for manually running test application host

--- src/BrowserHost.E2E/Properties/assemblyInfo.cs
+++ src/BrowserHost.E2E/Properties/assemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using BrowserHost.E2E.Infrastructure;
+using System.Runtime.CompilerServices;
 using Xunit.v3;
 
 [assembly: TestPipelineStartup(typeof(E2ETestPipelineStartup))]
 [assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
 [assembly: CaptureConsole]
+[assembly: InternalsVisibleTo("BrowserHost.E2E.Manual")]
